10c2201aacdd63fd7e1a50b5041bbea09ad5fe74
Rommmu <tonty1222@naver.com>
2023-10-20 23:35:27 +0900

    Modify settings.py :: add allowed_hosts

--- workshop_prj/settings.py
+++ workshop_prj/settings.py
@@ -37,7 +37,10 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1', # 런서버를 위해
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
