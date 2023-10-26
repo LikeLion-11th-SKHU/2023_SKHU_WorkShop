a42b902323a50496fc1bda33c43d9e30562b7e87
CMH <skdaks5701@naver.com>
2023-10-26 13:54:40 +0900

    fix:like_count

--- workshop_main/views.py
+++ workshop_main/views.py
@@ -28,8 +28,16 @@ def guestbook(request):
             workshop = WorkShop.objects.get(pk=workshop_id)
             
             if action == 'like':
-                workshop.likes += 1
-            workshop.save()
+                # 사용자가 이미 이 워크샵에 좋아요를 눌렀는지 확인
+                if 'liked_workshops' not in request.session:
+                    request.session['liked_workshops'] = []
+
+                if workshop_id not in request.session['liked_workshops']:
+                    workshop.likes += 1
+                    workshop.save()
+                    # 해당 워크샵을 좋아요 표시한 것으로 표시
+                    request.session['liked_workshops'].append(workshop_id)
+                    request.session.modified = True  # 세션 저장
 
     # 워크샵 생성
     if request.method == 'POST':
